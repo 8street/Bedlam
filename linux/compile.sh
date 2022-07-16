29e4d8149d52df431e382e2849c0f60063ea7fc9
Nikolay Ekaterinin <nikolai_e@rambler.ru>
2022-07-16 09:54:01 +0300

    Add linux build

---
+++ linux/compile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Delete previous version
+rm bedlam
+
+mkdir o
+cd o || exit 1
+rm -rf -- *
+
+echo                                          
+echo ============= run clang++-12 compiler =============
+clang++-12 -m32 -c ../../CPP_sources/*.cpp -std=c++17 $(pkg-config --cflags SDL2_mixer sdl2)
+
+echo                                          
+echo ============= run jwasm compiler =============
+jwasm -elf -c -zcw ../../ASM_sources/bedlam2.asm
+
+cd ..
+
+echo                                          
+echo ============= run ld linker =============
+clang++-12 -m32 o/*.o -o bedlam -lstdc++fs $(pkg-config --libs SDL2_mixer sdl2)
+
+chmod 755 bedlam
